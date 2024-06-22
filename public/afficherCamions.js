281de18929fc5e55109f362b005a8f07d6a80bf6
Elyoussf <strangeamazigh1968@gmail.com>
2024-06-22 02:53:06 +0100

    changed UI

--- public/afficherCamions.js
+++ public/afficherCamions.js
@@ -1,4 +1,4 @@
-const address = "https://705f-196-81-86-24.ngrok-free.app"
+const address = "https://705f-196-81-86-24.ngrok-free.app";
 const ar=[];
 document.addEventListener('DOMContentLoaded', async () => {
     try {
