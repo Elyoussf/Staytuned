5d718f24c4383dc45e1226f8b7606fee14408b65
Elyoussf <strangeamazigh1968@gmail.com>
2024-06-23 17:04:12 +0100

    life is good

--- index.js
+++ index.js
@@ -1,52 +1,49 @@
 const express = require('express');
-const nodemailer = require('nodemailer');
-const cookieParser = require('cookie-parser');
-const Admin = require('./models/admin')
 const bodyParser = require('body-parser');
-const path = require('path')
-const Email = require('./models/Email')
+const path = require('path');
 const cors = require('cors');
+const dotenv = require('dotenv');
 const connectDB = require('./database/db');
 const Camion = require('./models/Camion');
-const dotenv = require('dotenv')
-
-const {send} = require('./alertesPreventives/alerte');
-const authController= require('./controllers/auth');
+const Admin = require('./models/admin');
+const Email = require('./models/Email');
+const { send } = require('./alertesPreventives/alerte');
+const authController = require('./controllers/auth');
 const auth = require('./middleware/auth');
-const { Console } = require('console');
+
 const app = express();
-const port = process.env.PORT
-dotenv.config()
+const port = process.env.PORT || 3000;
+
+dotenv.config();
 connectDB();
 
+// Middleware
 app.use(bodyParser.json());
-app.use(cookieParser())
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors()); // Enable CORS for all routes
 
-app.get('/',function (req,res){
-res.sendFile(path.join(__dirname,'public','landPage.html'))
-})
+// Routes
 
-app.use(cors());
+// Landing page route
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'public', 'landPage.html'));
+});
+
 // Create a new camion
-app.post('/camions',auth,async (req, res) => {
-  
+app.post('/camions', auth, async (req, res) => {
     try {
-        const { immatriculation, type, kilometrage, dernierVidangeDate, dernierVidangeKilometrage ,cout,details} = req.body;
-        console.log(cout);
+        const { immatriculation, type, kilometrage, dernierVidangeDate, dernierVidangeKilometrage, cout, details } = req.body;
         const nouveauCamion = new Camion({
             immatriculation,
             type,
             kilometrage,
-            vidanges: [{ date: dernierVidangeDate, kilometrage: dernierVidangeKilometrage,cout:cout,details:details }],
-            
+            vidanges: [{ date: dernierVidangeDate, kilometrage: dernierVidangeKilometrage, cout, details }],
         });
-        
+
         await nouveauCamion.save();
-        console.log("received")
+        console.log("Camion ajouté avec succès");
         res.status(200).json({ success: true });
     } catch (err) {
         res.status(500).json({ error: err.message });
@@ -54,24 +51,22 @@ app.post('/camions',auth,async (req, res) => {
 });
 
 // Update camion using POST
-app.post('/camions/update',auth, async (req, res) => {
-    
+app.post('/camions/update', auth, async (req, res) => {
     try {
-        const { immatriculation, type, kilometrage, dernierVidangeDate, dernierVidangeKilometrage ,cout,details} = req.body;
+        const { immatriculation, type, kilometrage, dernierVidangeDate, dernierVidangeKilometrage, cout, details } = req.body;
         const camion = await Camion.findOneAndUpdate(
             { immatriculation },
             {
                 type,
                 kilometrage,
-                $push: { vidanges: { date: dernierVidangeDate, kilometrage: dernierVidangeKilometrage,cout:cout,details:details } },
-                     
-              },
+                $push: { vidanges: { date: dernierVidangeDate, kilometrage: dernierVidangeKilometrage, cout, details } },
+            },
             { new: true }
         );
         if (!camion) {
             return res.status(404).json({ error: 'Camion non trouvé' });
         }
-        console.log("Update request received");
+        console.log("Camion mis à jour avec succès");
         res.status(200).json(camion);
     } catch (err) {
         res.status(500).json({ error: err.message });
@@ -79,7 +74,7 @@ app.post('/camions/update',auth, async (req, res) => {
 });
 
 // Get all camions
-app.get('/camions', auth,async (req, res) => {
+app.get('/camions', auth, async (req, res) => {
     try {
         const camions = await Camion.find();
         res.status(200).json(camions);
@@ -89,7 +84,7 @@ app.get('/camions', auth,async (req, res) => {
 });
 
 // Get a camion by immatriculation
-app.get('/camions/:immatriculation', auth,async (req, res) => {
+app.get('/camions/:immatriculation', auth, async (req, res) => {
     try {
         const camion = await Camion.findOne({ immatriculation: req.params.immatriculation });
         if (!camion) {
@@ -102,7 +97,7 @@ app.get('/camions/:immatriculation', auth,async (req, res) => {
 });
 
 // Delete a camion by immatriculation
-app.delete('/camions/:immatriculation', auth,async (req, res) => {
+app.delete('/camions/:immatriculation', auth, async (req, res) => {
     try {
         const camion = await Camion.findOneAndDelete({ immatriculation: req.params.immatriculation });
         if (!camion) {
@@ -113,13 +108,17 @@ app.delete('/camions/:immatriculation', auth,async (req, res) => {
         res.status(500).json({ error: err.message });
     }
 });
-app.post("/send-email",auth,send);
 
+// Endpoint to send emails
+app.post('/send-email', auth, send);
+
+// Authentication endpoints
 app.post('/api/register', authController.register);
 app.post('/api/login', authController.login);
 app.post('/api/logout', authController.logout);
 
-app.post('/api/getCoutByYear',auth,async (req, res) => {
+// Get costs by year endpoint
+app.post('/api/getCoutByYear', auth, async (req, res) => {
     const { year } = req.body;
 
     try {
@@ -127,7 +126,7 @@ app.post('/api/getCoutByYear',auth,async (req, res) => {
         const endDate = new Date(`${year}-12-31`);
 
         const camions = await Camion.find({ 'vidanges.date': { $gte: startDate, $lte: endDate } });
-        
+
         const result = camions.reduce((acc, camion) => {
             camion.vidanges.forEach(vidange => {
                 if (vidange.date >= startDate && vidange.date <= endDate) {
@@ -146,7 +145,7 @@ app.post('/api/getCoutByYear',auth,async (req, res) => {
     }
 });
 
-// access admin space
+// Endpoint to verify admin key
 app.post('/verify-key', async (req, res) => {
     const { secret_key } = req.body;
 
@@ -155,34 +154,31 @@ app.post('/verify-key', async (req, res) => {
     }
 
     try {
-   
         const admin = await Admin.findOne();
         if (!admin) {
             return res.status(404).send('Admin data not found');
         }
         if (admin.compareSecretKey(secret_key)) {
-            return res.status(200).json({ success: true, message: 'Secret key verified' });
+            return res.status(200).json({ success: true, message: 'Clé secrète vérifiée' });
         } else {
-            return res.status(500).json({ success: false, error: 'Server error' });
+            return res.status(500).json({ success: false, error: 'Erreur de serveur' });
         }
     } catch (err) {
         console.error(err);
-        return res.status(500).send('Server error');
+        return res.status(500).send('Erreur de serveur');
     }
 });
-app.post('/change-email',async (req, res) => {
+
+// Endpoint to change email
+app.post('/change-email', async (req, res) => {
     const { receiver_email } = req.body;
     const newEmail = new Email({
-        receiver_email:receiver_email
-    })
-  
-    await newEmail.save()
-    console.log("saved")
-    res.status(200).json({ success: true, message: 'Email added successfully' });
-    
+        receiver_email: receiver_email
+    });
+
+    await newEmail.save();
+    console.log("Email ajouté avec succès");
+    res.status(200).json({ success: true, message: 'Email ajouté avec succès' });
 });
 
-
-
 app.listen(port, () => console.log(`Serveur démarré sur le port ${port}`));
-
